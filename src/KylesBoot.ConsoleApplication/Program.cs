df5cb3e54860cd55660b6354006db69e7f51c992
MarcusOtter <github@otterstrom.dev>
2020-11-04 19:01:13 +0100

    Add basic types and set up dependency injection

---
+++ src/KylesBoot.ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using KylesBoot.Logic;
+
+namespace KylesBoot.ConsoleApplication
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            var serviceProvider = GetServiceProvider();
+            var bot = serviceProvider.GetService<IKylesBootDiscordBot>();
+
+            await bot.StartAsync();
+
+            // Wait indefinitely for now
+            await Task.Delay(-1);
+        }
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            // Add new types here
+            var serviceCollection = new ServiceCollection()
+                .AddSingleton<IKylesBootDiscordBot, KylesBootDiscordBot>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ src/KylesBoot.Logic/KylesBootDiscordBot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KylesBoot.Logic
+{
+    public interface IKylesBootDiscordBot
+    {
+        Task StartAsync();
+        Task StopAsync();
+    }
+
+    public class KylesBootDiscordBot : IKylesBootDiscordBot
+    {
+        public Task StartAsync()
+        {
+            Console.WriteLine("Started!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
